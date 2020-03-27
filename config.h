029a8b9df614e1c01d79d88457e006ef9925bf06
viz <visuwesh@tutanota.com>
2020-03-27 16:12:16 +0530

    change border colour

--- config.h
+++ config.h
@@ -5,9 +5,7 @@ static int topbar = 1;
 static int fuzzy = 1;
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-/* static const char *fonts[] = {
-    "Share Tech Mono:pixelsize=13"
-};*/
+static const char *fonts[] = { "monospace:pixelsize=12" };
 
 /* -p  option; prompt to the left of input field */
 static const char *prompt      = NULL;
@@ -15,7 +13,6 @@ static const char *prompt      = NULL;
 /* border width in pixels. can be overriden with the -bp flag */
 static unsigned int borderpx   = 0;
 
-static const char *fonts[] = { "monospace:pixelsize=12" };
 static const char *colors[SchemeLast][2] = {
     /*                    fg               bg     */
     [SchemeNorm]    =  { "#1c1e1d",      "#e7e7d4" },
